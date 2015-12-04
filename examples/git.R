1e0d38e366cbc108ca81cb9fdda90757b77f4cd1
helske <jouni.helske@jyu.fi>
2015-12-04 10:24:33 +0200

    "another commit now with proper identity"

---
+++ examples/git.R
@@ -0,0 +1,6 @@
+# make changes to files, change x <- 1 to x <- 2
+x <- 1
+
+# command "git add ." stages changes
+# command "git commit -m "commit message"" updates the local git repository
+# command "git push origin master" pushes the updates to remote repository
