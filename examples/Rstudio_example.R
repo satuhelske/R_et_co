527289023d5741efd637dadf1d2e98f6fff24f6c
Jouni Helske <jouni.helske@jyu.fi>
2015-12-04 13:27:04 +0200

    change

--- examples/Rstudio_example.R
+++ examples/Rstudio_example.R
@@ -1,10 +1,11 @@
 
 syntax_higlighting <- function(something = "abc", number = 123){
 
-  there_is_no_space<-something
+  there_is_no_space <- something
 
   for(i in 1:10){
-    missing_parenthesis(
+    missing_parenthesis()
+
 
   }
   number
